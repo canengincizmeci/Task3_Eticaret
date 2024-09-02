25224305b44b72abc6ca067220669be5693adaec
Can Engin Çizmeci <canncizmeci@gmail.com>
2024-09-02 10:15:52 +0300

    .

---
+++ SatisPaneli/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using DB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SatisPaneli.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly Task3RealEcommerceContext _context;
+        public UserController(ILogger<UserController> logger, Task3RealEcommerceContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
